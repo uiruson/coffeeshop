5cbe62066bffe37df1b2d26c9f8f5cd65b308171
uiruson88 <uiruson@gmail.com>
2014-05-31 14:32:52 -0700

    added vendor.css.scss

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Coffeeshop
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
-    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
   end
 end
